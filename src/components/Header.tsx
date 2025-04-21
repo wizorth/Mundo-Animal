0ab87fc39c33bc45fe8126142131ef576ccab18d
wizorth <wilzorjho@gmail.com>
2025-04-21 10:33:30 -0400

    mensaje de prueba sinc git hub

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,9 @@ export default function Header() {
         <img src="/images/logo_mundoAnimal.png" className="mr-3 h-12 sm:h-16" alt="Logo" />
         <span className="self-center whitespace-nowrap text-5xl font-semibold text-white text-outline drop-shadow-md">Mundo Animal</span>
       </Navbar.Brand>
-      <Navbar.Toggle className="text-white" />
+      <Navbar.Toggle />
       <Navbar.Collapse>
-        <Navbar.Link href="#home" active className="text-white drop-shadow-md text-outline">
+        <Navbar.Link href="#home" active className="text-white ">
           Home
         </Navbar.Link>
         <Navbar.Link href="#about" active className="text-white drop-shadow-md text-outline">
